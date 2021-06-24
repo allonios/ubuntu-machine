ae4ee1bab254830b7fef227b383910a726a3e356
Allonios <fareck.allony.mail@gmail.com>
2021-06-24 20:49:45 +0300

    added babi editor and tkinter

--- scripts/main_packages.sh
+++ scripts/main_packages.sh
@@ -1,6 +1,16 @@
 #!/usr/bin/env bash
 
-sudo apt install dconf-editor compizconfig-settings-manager git python3.8 python3-pip software-properties-common python-dev python3-dev python3.8-dev python3.8-venv libpq-dev build-essential snapd python-setuptools vim vlc zip unzip sqlitebrowser qbittorrent openvpn network-manager-openvpn-gnome build-essential libssl-dev -y
+# main packages
+###############################################################################################################################################
+sudo add-apt-repository ppa:git-core/ppa -y
+sudo apt install git
+sudo apt install dconf-editor compizconfig-settings-manager python3.8 python3-pip software-properties-common python-dev python3-dev python3.8-dev python3.8-venv libpq-dev build-essential snapd python-setuptools vim vlc zip unzip rar unrar default-libmysqlclient-dev sqlitebrowser qbittorrent openvpn network-manager-openvpn-gnome build-essential libssl-dev -y
+###############################################################################################################################################
+
+# babi editor
+###############################################################################################################################################
+python3 -m pip install babi
+###############################################################################################################################################
 
 # tkinter
 ###############################################################################################################################################
@@ -20,10 +30,12 @@ sudo apt install papirus-icon-theme -y
 ###############################################################################################################################################
 
 # git aliases
+###############################################################################################################################################
 git config --global alias.co checkout
 git config --global alias.br branch
 git config --global alias.ci commit
 git config --global alias.st status
+###############################################################################################################################################
 
 # melody player
 ###############################################################################################################################################
--- scripts/unpack_files.sh
+++ scripts/unpack_files.sh
@@ -4,3 +4,6 @@ mv ../.themes ~/
 mv ../.bash_aliases ~/
 mv ../.bashrc ~/
 mv ../tensor_test.py ~/
+# babi theme setup
+mkdir mkdir ~/.config/babi
+mv ../theme.json ~/.config/babi
